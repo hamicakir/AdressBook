6abec4e1ae21b8fe2181f284a6b3a374da82ad49
hamicakir <hmi.cakir@gmail.com>
2017-10-22 01:00:20 +0300

    Bootstrap add and adressbook view add

--- index.php
+++ index.php
@@ -6,8 +6,67 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Adress Book | Hami ÇAKIR</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="/css/main.css"/>
 </head>
 <body>
+<header class="header">
+    <div class="container">
+        <nav class="navbar">
+            <a class="navbar-brand align-items-center" href="index.php">AdressBOOK</a>
+        </nav>
+    </div>
+
+</header>
+<div class="content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="adder">
+                    <form>
+                        <label class="label-file" for="file"></label>
+                        <input id="file" type="file" name="person_image" />
+                        <input type="text" name="person_name" placeholder="Name Surname" />
+                        <input type="tel" name="person_phone" placeholder="Phone Number" />
+                        <input type="email" name="person_email" placeholder="E-mail" />
+                        <textarea rows="10" placeholder="Adress"></textarea>
+                        <input type="submit" value="Kaydet"/>
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="adder">
+                    <form>
+                        <label class="label-file" for="file"></label>
+                        <input id="file" type="file" name="person_image" />
+                        <input type="text" name="person_name" placeholder="Name Surname" />
+                        <input type="tel" name="person_phone" placeholder="Phone Number" />
+                        <input type="email" name="person_email" placeholder="E-mail" />
+                        <textarea rows="10" placeholder="Adress"></textarea>
+                        <input type="submit" value="Kaydet"/>
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="adder">
+                    <form>
+                        <label class="label-file" for="file"></label>
+                        <input id="file" type="file" name="person_image" />
+                        <input type="text" name="person_name" placeholder="Name Surname" />
+                        <input type="tel" name="person_phone" placeholder="Phone Number" />
+                        <input type="email" name="person_email" placeholder="E-mail" />
+                        <textarea rows="10" placeholder="Adress"></textarea>
+                        <input type="submit" value="Kaydet"/>
+                    </form>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
+
+
 
 
 
