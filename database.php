b50226220c9f13da21eb42697961c5041d4f0ac3
hamicakir <hmi.cakir@gmail.com>
2017-10-21 14:26:07 +0300

    File system created.

---
+++ database.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hmica
+ * Date: 21.10.2017
+ * Time: 14:00
+ */
---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Adress Book | Hami ÇAKIR</title>
+</head>
+<body>
+
+
+
+</body>
+</html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hmica
+ * Date: 21.10.2017
+ * Time: 14:00
+ */
