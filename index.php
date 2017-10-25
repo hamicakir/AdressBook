976d172025a91bd9acd906701cc0bb7ddca0b0d2
hamicakir <hmi.cakir@gmail.com>
2017-10-25 16:09:44 +0300

    İonicons add and search search button add

--- index.php
+++ index.php
@@ -5,18 +5,22 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="theme-color" content="#1e76d1">
     <title>Adress Book | Hami ÇAKIR</title>
     <link rel="stylesheet" href="css/bootstrap.min.css"/>
     <link rel="stylesheet" href="/css/main.css"/>
+    <link rel="stylesheet" href="/css/ionicons.min.css"/>
 </head>
 <body>
 <header class="header">
     <div class="container">
         <nav class="navbar navbar-expand-lg">
-            <a class="navbar-brand align-items-center" href="index.php">AdressBOOK</a>
-            <ul class="navbar-nav mr-auto">
+            <a class="navbar-brand align-items-center" href="index.php">Adress<b>BOOK</b></a>
+            <ul class="navbar-nav ml-auto">
+
                 <li class="nav-item">
-                    <a class="nav-link" href="#"><i class="" </a>
+
+                    <a href="javascript:void(0);" class="nav-link" onclick="openSearch()"><i class="ion ion-search"></i></a>
                 </li>
 
             </ul>
@@ -24,13 +28,17 @@
     </div>
 
 </header>
+<div id="search">
+    <a href="javascript:void(0);" onclick="closeSearch()"><i class="ion ion-close-round"></i></a>
+    <input type="text" class="search-input" name="search" />
+</div>
 <div class="content">
     <div class="container">
         <div class="row">
             <div class="col-md-4">
                 <div class="adder">
                     <form>
-                        <label class="label-file" for="file"></label>
+                        <label class="label-file" for="file"><i class="ion ion-camera"></i> </label>
                         <input id="file" type="file" name="person_image" accept="image/jpeg,image/png,image/jpg" />
                         <input type="text" name="person_name" placeholder="Name Surname" />
                         <input type="tel" name="person_phone" placeholder="Phone Number" />
@@ -43,7 +51,7 @@
             <div class="col-md-4">
                 <div class="adder">
                     <form>
-                        <label class="label-file" for="file"></label>
+                        <label class="label-file" for="file"><i class="ion ion-camera"></i> </label>
                         <input id="file" type="file" name="person_image" accept="image/jpeg,image/png,image/jpg" />
                         <input type="text" name="person_name" placeholder="Name Surname" />
                         <input type="tel" name="person_phone" placeholder="Phone Number" />
@@ -56,7 +64,7 @@
             <div class="col-md-4">
                 <div class="adder">
                     <form>
-                        <label class="label-file" for="file"></label>
+                        <label class="label-file" for="file"><i class="ion ion-camera"></i> </label>
                         <input id="file" type="file" name="person_image" accept="image/jpeg,image/png,image/jpg" />
                         <input type="text" name="person_name" placeholder="Name Surname" />
                         <input type="tel" name="person_phone" placeholder="Phone Number" />
@@ -64,6 +72,11 @@
                         <textarea rows="10" placeholder="Adress"></textarea>
                         <input type="submit" value="Kaydet"/>
                     </form>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="adder">
+                    <a class="new-contact" href="#"><i class="ion ion-android-add-circle"></i><span>New Contact</span></a>
                 </div>
             </div>
 
@@ -74,8 +87,7 @@
 
 
 
-
-
+<script type="text/javascript" src="js/main.js"></script>
 </body>
 </html>
 
---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+var searchArea = document.getElementById("search");
+
+function openSearch() {
+    searchArea.style.visibility = "visible";
+}
+function closeSearch() {
+    searchArea.style.visibility = "hidden";
+}
